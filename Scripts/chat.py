26a827cdc35e1336d00e0528b42d63b49458e862
mikhailstasyuk <78797945+mikhailstasyuk@users.noreply.github.com>
2024-02-02 23:27:16 +0400

    Added a request script
    Requests a model reply from the LM Studio Local Server

--- Scripts/chat.py
+++ Scripts/chat.py
@@ -1,18 +1,47 @@
+import json
+from datetime import datetime
 from openai import OpenAI
 from argparse import ArgumentParser
 
-def main(kwargs):
+def generate_fname():
+    # Get the current time
+    now = datetime.now()
+
+    # Format the date and time
+    timestamp = now.strftime("%Y%m%d-%H%M%S")
+
+    # Create a filename with the current date and time
+    filename = f"file-{timestamp}.json"
+    return filename
+
+def dump_reply(data):
+    # Generate a filename
+    filename = generate_fname()
+
+    # Save LM Studio reply to a json file
+    with open(filename, 'w') as f_out:
+        json.dump(data, f_out)
+
+def request(kwargs):
 	# Point to the local server
-	client = OpenAI(base_url=f"http://localhost:1234/v1", api_key="not-needed")
-	completion = client.chat.completions.create(
+    client = OpenAI(base_url=f"http://localhost:1234/v1", api_key="not-needed")
+    completion = client.chat.completions.create(
         model="local-model", # this field is currently unused
         messages=[
             {"role": "system", "content": "Always answer in rhymes."},
             {"role": "user", "content": "Introduce yourself."}
         ],
         temperature=0.7,
-	)
-	return completion.choices[0].message
+    )
+    
+    return completion.choices[0].message.content
+
+def main(kwargs):
+    # Request a model reply from the LM Studio Local Server
+    response = request(kwargs)
+
+    # Save the reply to a file
+    dump_reply(response)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
